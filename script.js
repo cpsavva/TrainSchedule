b038ae7ce4f6ddbbed74ad8638d1ed89ad955ee0
cpsavva <csoriasavva@gmail.com>
2017-01-24 14:56:26 -0500

    added firebase account to page

---
+++ script.js
@@ -0,0 +1,14 @@
+//// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCbuMscL4DhabjM-Bv3z8GMRJOZk2uMcAI",
+    authDomain: "trainschedule-9c7fc.firebaseapp.com",
+    databaseURL: "https://trainschedule-9c7fc.firebaseio.com",
+    storageBucket: "trainschedule-9c7fc.appspot.com",
+    messagingSenderId: "931627838720"
+  };
+  firebase.initializeApp(config);
+
+
+ var trainDatabase = firebase.database();
+
+ 
